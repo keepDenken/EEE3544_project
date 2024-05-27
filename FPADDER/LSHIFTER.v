5a450cb649fe39ff409379562488ffa9def92888
Min Chan Kim <gmtgr0910@yonsei.ac.kr>
2024-05-28 03:13:55 +0900

    Remove unnecessary files

--- FPADDER/LSHIFTER.v
+++
@@ -1,7 +0,0 @@
-module LSHIFTER (
-    input  wire [27:0] M,
-    input  wire [4:0] NUM_0S,
-    output wire [26:0] M_NORM,
-)
-
-assign M_NORM = (M>>NUM_0S)[27:1];
--- FPADDER/RSHIFTER.v
+++
@@ -1,9 +0,0 @@
-module RSHIFTER(
-    input  wire [26:0] M_SMALL,
-    input  wire [7:0]  EXP_DIFF,
-    output wire [26:0] M_EFF
-);
-
-assign M_EFF = M_SMALL >> EXP_DIFF;
-
-endmodule
